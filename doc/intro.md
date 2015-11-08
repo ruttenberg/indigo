bbd73ff4a96ebe97c768066a02ab8df962b0d50c
Jonathan Ruttenberg <jpr@OnCallAir6.local>
2015-11-08 18:39:58 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to indigo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/indigo/core_test.clj
@@ -0,0 +1,7 @@
+(ns indigo.core-test
+  (:require [clojure.test :refer :all]
+            [indigo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
