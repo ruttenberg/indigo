aed238883e946a460e35d6400a090ea90402319d
Jonathan Ruttenberg <ruttenberg@stanfordalumni.org>
2015-11-18 08:13:17 -0500

    Tests and demo output.

--- src/indigo/core.clj
+++ src/indigo/core.clj
@@ -1,6 +1,22 @@
 (ns indigo.core
   (:gen-class))
 
+(use 'clojure.pprint)
+
+;-----
+(defmacro futures
+  [n & exprs]
+  (vec (for [_ (range n)
+             expr exprs]
+         `(future ~expr))))
+
+
+;-----
+(defmacro wait-futures
+  [& args]
+  `(doseq [f# (futures ~@args)]
+     @f#))
+
 (def kv-store (ref {}))
 
 (defn get-value [key]
@@ -56,11 +72,37 @@
                (set-value-for-key key (pop value-at-key))
                (peek value-at-key))))))
 
+(defn dump []
+  (println "value of KV STORE")
+  (pprint (deref kv-store))
+  (println)
+  )
+
+(defn running [val]
+  (pprint 'RUNNING...)
+  (pprint val)
+  (println))
+
+(defn random-sleep []
+  (Thread/sleep (rand-int 500)))
+
+(defn do-with-random-sleep [func & rest]
+  (random-sleep)
+  (apply func rest) )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
+
+  (dump)
+
+  (running (quote (add-to-kv-store (list
+                     '("string" "string-value")
+                     (list "list" (apply list (range 1 10)))
+                     (list "map" {"key1" "value1"
+                                  "key2" "value2"
+                                  "key3" "value3"})))))
   (add-to-kv-store (list
                      '("string" "string-value")
                      (list "list" (apply list (range 1 10)))
@@ -68,22 +110,43 @@
                                   "key2" "value2"
                                   "key3" "value3"})))
 
+  (dump)
+
+  (running '(set-value-for-key "foo" "bar"))
   (set-value-for-key "foo" "bar")
-  (println (deref kv-store))
+  (dump)
 
+  (running '(hset "map" "key3" "newval"))
   (hset "map" "key3" "newval")
-  (println)
-  (println (deref kv-store))
+  (dump)
 
+  (println "hset on a list is a no-op")
+  (running '(hset "list" "key3" "newval"))
   (hset "list" "key3" "newval")
-  (println)
-  (println (deref kv-store))
+  (dump)
 
+  (println "hset on a string is a no-op")
+  (running '(hset "string" "key3" "newval"))
   (hset "string" "key3" "newval")
-  (println) (println (deref kv-store))
+  (dump)
+
+  (running (quote
+             (println (get-value "string"))
+             (println (get-value "list"))
+             (println (get-value "map"))
+             ))
+
   (println (get-value "string"))
   (println (get-value "list"))
   (println (get-value "map"))
+  (println)
+  (dump)
+
+  (running (quote
+               (let [pop-result (lpop "list")]
+    (println)
+    (println pop-result)
+    (println (get-value "list")))))
 
   (let [pop-result (lpop "list")]
     (println)
@@ -91,6 +154,22 @@
     (println (get-value "list")))
 
   (println)
-  (println (deref kv-store)))
+  (dump)
+
+  (running (quote
+    (time (wait-futures 1
+                      (lpop "list")
+                      (get-value "list")
+                     (set-value-for-key "foo" "fighters")))))
+
+  (time (wait-futures 1
+                      (do-with-random-sleep lpop "list")
+                      (pprint (do-with-random-sleep get-value "list"))
+                      (do-with-random-sleep set-value-for-key "foo" "fighters")))
+
+  (dump)
+
+  (System/exit 0)
+  )
 
 
